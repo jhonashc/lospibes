80d75047e6b847716ff204583b8e2b7bfd016304
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-10 02:38:19 -0500

    common model updated: chip item added

---
+++ app/src/main/java/com/example/lospibes/common/domain/model/ChipItem.kt
@@ -0,0 +1,8 @@
+package com.example.lospibes.common.domain.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class ChipItem(
+    val name: String,
+    val icon: ImageVector? = null
+)
