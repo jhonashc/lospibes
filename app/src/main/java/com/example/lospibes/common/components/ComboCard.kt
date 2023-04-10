ab13455c0bb6e1d58d81e473476dd99d3ce9608f
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-10 16:49:40 -0500

    common components updated: standard card implemented in combo and product card

--- app/src/main/java/com/example/lospibes/common/components/ComboCard.kt
+++ app/src/main/java/com/example/lospibes/common/components/ComboCard.kt
@@ -1,25 +1,7 @@
 package com.example.lospibes.common.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import com.example.lospibes.common.domain.model.CardItem
 import com.example.lospibes.common.domain.model.Combo
 
 @Composable
@@ -28,103 +10,14 @@ fun ComboCard(
     isFavorite: Boolean = false,
     onClick: () -> Unit
 ) {
-    val favoriteIcon = if (isFavorite)
-        Icons.Filled.Favorite else
-        Icons.Filled.FavoriteBorder
-
-    Card(
-        modifier = Modifier
-            .width(200.dp)
-            .wrapContentHeight()
-            .clickable(onClick = onClick),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 1.dp
+    StandardCard(
+        cardItem = CardItem(
+            name = combo.name,
+            description = combo.description,
+            imageUrl = combo.imageUrl,
+            price = combo.price
         ),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.background
-        ),
-    ) {
-        Box(
-            modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            AsyncImage(
-                modifier = Modifier.size(140.dp),
-                model = combo.imageUrl,
-                contentDescription = combo.name,
-                contentScale = ContentScale.Fit,
-            )
-
-            Icon(
-                modifier = Modifier
-                    .padding(15.dp)
-                    .size(25.dp)
-                    .align(Alignment.TopEnd),
-                imageVector = favoriteIcon,
-                contentDescription = "Favorite",
-                tint = MaterialTheme.colorScheme.primary
-            )
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(15.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = combo.name,
-                style = MaterialTheme.typography.titleMedium,
-                overflow = TextOverflow.Ellipsis,
-                color = MaterialTheme.colorScheme.primary,
-                maxLines = 1
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Text(
-                modifier = Modifier.alpha(0.8f),
-                text = combo.description ?: "",
-                fontWeight = FontWeight.Normal,
-                style = MaterialTheme.typography.bodySmall,
-                overflow = TextOverflow.Ellipsis,
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colorScheme.outline,
-                maxLines = 2,
-            )
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 12.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "$${combo.price}",
-                    fontWeight = FontWeight.SemiBold,
-                    style = MaterialTheme.typography.titleMedium,
-                    color = MaterialTheme.colorScheme.primary
-                )
-
-                Box(
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(CircleShape)
-                        .background(MaterialTheme.colorScheme.primary)
-                ) {
-                    IconButton(
-                        onClick = {}
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.Add,
-                            contentDescription = "Add",
-                            tint = MaterialTheme.colorScheme.background
-                        )
-                    }
-                }
-            }
-        }
-    }
+        isFavorite = isFavorite,
+        onClick = onClick
+    )
 }
