5cba24e28242c36a2745668d7939d646d228fe15
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-10 17:23:10 -0500

    common components updated: new vector icons implemented

--- app/src/main/java/com/example/lospibes/common/components/StandardBottomAppBar.kt
+++ app/src/main/java/com/example/lospibes/common/components/StandardBottomAppBar.kt
@@ -39,7 +39,7 @@ fun StandardBottomAppBar(
                 },
                 icon = {
                     Icon(
-                        imageVector = navItem.icon,
+                        painter = navItem.icon,
                         contentDescription = navItem.name
                     )
                 },
