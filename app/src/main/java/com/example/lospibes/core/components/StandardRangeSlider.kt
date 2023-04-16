0b2da977a82f7b9e77eccbe996ed0bfba995e054
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-16 01:43:00 -0500

    core components updated: standard range slider added

---
+++ app/src/main/java/com/example/lospibes/core/components/StandardRangeSlider.kt
@@ -0,0 +1,24 @@
+package com.example.lospibes.core.components
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.RangeSlider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StandardRangeSlider(
+    modifier: Modifier = Modifier,
+    value: ClosedFloatingPointRange<Float>,
+    valueRange: ClosedFloatingPointRange<Float>,
+    onValueChange: (newValue: ClosedFloatingPointRange<Float>) -> Unit,
+    steps: Int
+) {
+    RangeSlider(
+        modifier = modifier,
+        value = value,
+        valueRange = valueRange,
+        onValueChange = onValueChange,
+        steps = steps
+    )
+}
