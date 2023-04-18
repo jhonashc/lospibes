5f026cbeeb8352286c0c25dfa5fad58632109984
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-18 02:09:23 -0500

    constants updated: categories endpoint added

--- app/src/main/java/com/example/lospibes/utils/Constants.kt
+++ app/src/main/java/com/example/lospibes/utils/Constants.kt
@@ -13,7 +13,8 @@ object Constants {
     const val DETAIL_GRAPH_ROUTE = "detail_graph"
 
     const val BASE_URL = BuildConfig.BASE_URL
-    const val PRODUCT_ENDPOINT = "products"
+    const val CATEGORIES = "categories"
+    const val PRODUCTS = "products"
 
     // Temporal
     private val date = Date()
@@ -22,55 +23,55 @@ object Constants {
         Category(
             id = "100",
             name = "Todas",
-            code = "\uD83D\uDCCC",
+            emojiCode = "\uD83D\uDCCC",
             createdAt = date,
             updatedAt = date
         ),
         Category(
             id = "1",
             name = "Hamburguesas",
-            code = "\uD83C\uDF54",
+            emojiCode = "\uD83C\uDF54",
             createdAt = date,
             updatedAt = date
         ),
         Category(
             id = "2",
             name = "Bebidas",
-            code = "\uD83E\uDD64",
+            emojiCode = "\uD83E\uDD64",
             createdAt = date,
             updatedAt = date
         ),
         Category(
             id = "3",
             name = "Pizzas",
-            code = "\uD83C\uDF55",
+            emojiCode = "\uD83C\uDF55",
             createdAt = date,
             updatedAt = date
         ), Category(
             id = "4",
             name = "Cócteles",
-            code = "\uD83C\uDF79",
+            emojiCode = "\uD83C\uDF79",
             createdAt = date,
             updatedAt = date
         ),
         Category(
             id = "5",
             name = "Hot dogs",
-            code = "\uD83C\uDF2D",
+            emojiCode = "\uD83C\uDF2D",
             createdAt = date,
             updatedAt = date
         ),
         Category(
             id = "6",
             name = "Tacos",
-            code = "\uD83C\uDF2E",
+            emojiCode = "\uD83C\uDF2E",
             createdAt = date,
             updatedAt = date
         ),
         Category(
             id = "46",
             name = "Alitas",
-            code = "\uD83C\uDF57",
+            emojiCode = "\uD83C\uDF57",
             createdAt = date,
             updatedAt = date
         )
@@ -84,7 +85,9 @@ object Constants {
             imageUrl = "https://burgerkingec.com/wp-content/uploads/2020/04/bbq-bacon-king-img.png",
             price = 3.50,
             stock = 100,
-            categories = categories
+            categories = categories,
+            createdAt = date,
+            updatedAt = date
         ),
         Product(
             id = "2",
@@ -93,7 +96,9 @@ object Constants {
             imageUrl = "https://pimagerepository.churchstexaschicken.com/73929f03-04d8-48ae-b690-de199b19aba8_actual.png",
             price = 6.50,
             stock = 100,
-            categories = categories
+            categories = categories,
+            createdAt = date,
+            updatedAt = date
         ),
         Product(
             id = "3",
@@ -102,7 +107,9 @@ object Constants {
             imageUrl = "https://static.wixstatic.com/media/f9728f_a8ffafb7270348918ac0f2cd81d1d27e~mv2_d_5184_3456_s_4_2.png/v1/fill/w_520,h_346,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Boneless_personal_recorte1.png",
             price = 6.50,
             stock = 100,
-            categories = categories
+            categories = categories,
+            createdAt = date,
+            updatedAt = date
         ),
         Product(
             id = "4",
@@ -111,7 +118,9 @@ object Constants {
             imageUrl = "https://underpizza-staging-image-storage.s3.us-east-2.amazonaws.com/sides/dEcc48jAvLAM5t2KY/1.png",
             price = 7.0,
             stock = 100,
-            categories = categories
+            categories = categories,
+            createdAt = date,
+            updatedAt = date
         ),
         Product(
             id = "5",
@@ -120,7 +129,9 @@ object Constants {
             imageUrl = "https://livingonthecheap.com/lotc-cms/wp-content/uploads/2017/07/unnamed-1-e1507716136762.png",
             price = 3.25,
             stock = 100,
-            categories = categories
+            categories = categories,
+            createdAt = date,
+            updatedAt = date
         ),
         Product(
             id = "6",
@@ -129,7 +140,9 @@ object Constants {
             imageUrl = "https://burgerkingec.com/wp-content/uploads/2020/05/Hersheys-Sundae-Pie-768x768.png",
             price = 3.0,
             stock = 100,
-            categories = categories
+            categories = categories,
+            createdAt = date,
+            updatedAt = date
         )
     )
 
@@ -141,7 +154,9 @@ object Constants {
             imageUrl = "https://www.hungryjacks.com.au/Upload/HJ/Media/UNO/HJ00571_WEB_Jack%CE%93COs-Fried-Chicken-classic_800X600_3.png",
             price = 3.0,
             stock = 100,
-            categories = categories
+            categories = categories,
+            createdAt = date,
+            updatedAt = date
         ),
         Product(
             id = "3",
@@ -150,7 +165,9 @@ object Constants {
             imageUrl = "https://burgerkingec.com/wp-content/uploads/2020/04/bbq-bacon-king-img.png",
             price = 3.50,
             stock = 100,
-            categories = categories
+            categories = categories,
+            createdAt = date,
+            updatedAt = date
         ),
         Product(
             id = "1",
@@ -159,7 +176,9 @@ object Constants {
             imageUrl = "https://olo-images-live.imgix.net/2e/2e44fc9d4ecd447ebb10f0f49810b198.png?auto=format%2Ccompress&q=60&cs=tinysrgb&w=528&h=352&fit=fill&fm=png32&bg=transparent&s=51cb9035dab2ac756a30f53e6b51b449",
             price = 6.50,
             stock = 100,
-            categories = categories
+            categories = categories,
+            createdAt = date,
+            updatedAt = date
         ),
     )
 
@@ -167,25 +186,25 @@ object Constants {
         Combo(
             id = "1",
             name = "Chicken Popcorn",
-            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus imperdiet odio, in semper risus faucibus ut. Praesent lacinia augue quis lacus posuere dictum. Morbi placerat libero felis, id gravida velit cursus ut. Quisque tempus bibendum urna, non maximus justo.",
+//            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus imperdiet odio, in semper risus faucibus ut. Praesent lacinia augue quis lacus posuere dictum. Morbi placerat libero felis, id gravida velit cursus ut. Quisque tempus bibendum urna, non maximus justo.",
             imageUrl = "https://images.getduna.com/2bb258f4-051d-4a46-9321-b0a7a857cd71/9e9dea90ac64f3a4_domicilio_54927_744x744_1675371715.png?d=300x300&format=webp",
-            price = 5.35,
-            products = products
+            price = 5,
+//            products = products
         ),
         Combo(
             id = "2",
             name = "Dynamite Burger Combo",
-            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus imperdiet odio, in semper risus faucibus ut. Praesent lacinia augue quis lacus posuere dictum. Morbi placerat libero felis, id gravida velit cursus ut. Quisque tempus bibendum urna, non maximus justo.",
+//            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus imperdiet odio, in semper risus faucibus ut. Praesent lacinia augue quis lacus posuere dictum. Morbi placerat libero felis, id gravida velit cursus ut. Quisque tempus bibendum urna, non maximus justo.",
             imageUrl = "https://kfcprodnecmsimage.azureedge.net/cmsimages/uae/imagestemp/900537.png",
             price = 6,
-            products = products
+//            products = products
         ), Combo(
             id = "3",
             name = "Chicken wings",
-            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus imperdiet odio, in semper risus faucibus ut. Praesent lacinia augue quis lacus posuere dictum. Morbi placerat libero felis, id gravida velit cursus ut. Quisque tempus bibendum urna, non maximus justo.",
+//            description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis faucibus imperdiet odio, in semper risus faucibus ut. Praesent lacinia augue quis lacus posuere dictum. Morbi placerat libero felis, id gravida velit cursus ut. Quisque tempus bibendum urna, non maximus justo.",
             imageUrl = "https://images.getduna.com/2bb258f4-051d-4a46-9321-b0a7a857cd71/9e9dea90ac64f3a4_domicilio_499_744x744.png?d=300x300&format=webp",
             price = 4,
-            products = products
+//            products = products
         )
     )
 }
