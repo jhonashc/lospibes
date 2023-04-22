629f945a853aa623b045f08d98aadceb6a3b299b
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-22 15:34:37 -0500

    home feature updated: combo response updated in data response

--- app/src/main/java/com/example/lospibes/features/home/data/dto/response/ComboResponse.kt
+++ app/src/main/java/com/example/lospibes/features/home/data/dto/response/ComboResponse.kt
@@ -2,7 +2,12 @@ package com.example.lospibes.features.home.data.dto.response
 
 import com.example.lospibes.features.home.domain.model.Combo
 
-data class ComboResponse(
+data class CombosResponse(
     val status: Boolean,
     val data: List<Combo>
+)
+
+data class ComboResponse(
+    val status: Boolean,
+    val data: Combo
 )
