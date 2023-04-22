42fb5893d4a1286f4b0c65a02d335667a06f3026
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-22 01:25:32 -0500

    home feature updated: initial product view model added in product screen

---
+++ app/src/main/java/com/example/lospibes/features/home/presentation/product/presentation/ProductViewModel.kt
@@ -0,0 +1,107 @@
+package com.example.lospibes.features.home.presentation.product.presentation
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.lospibes.features.home.data.dto.query.GetProductsQueryDto
+import com.example.lospibes.features.home.domain.use_case.product.ProductUseCase
+import com.example.lospibes.utils.NetworkResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProductViewModel @Inject constructor(
+    private val productUseCase: ProductUseCase,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    private val _state = MutableStateFlow(ProductState())
+    val state = _state.asStateFlow()
+
+    private val productId: String = savedStateHandle["productId"] ?: ""
+
+    init {
+        getProductById(
+            productId = productId
+        )
+    }
+
+    private fun getProductById(
+        productId: String
+    ) {
+        viewModelScope.launch {
+            productUseCase.getProductById(
+                id = productId
+            ).collect { res ->
+                when (res) {
+                    is NetworkResult.Loading -> {
+                        _state.update {
+                            it.copy(
+                                isProductLoading = true
+                            )
+                        }
+                    }
+
+                    is NetworkResult.Success -> {
+                        _state.update {
+                            it.copy(
+                                product = res.data?.data,
+                                isProductLoading = false
+                            )
+                        }
+                    }
+
+                    is NetworkResult.Error -> {
+                        _state.update {
+                            it.copy(
+                                message = res.message,
+                                isProductLoading = false
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun getSimilarProducts(
+        getProductsQueryDto: GetProductsQueryDto? = null
+    ) {
+        viewModelScope.launch {
+            productUseCase.getProducts(
+                getProductsQueryDto = getProductsQueryDto
+            ).collect { res ->
+                when (res) {
+                    is NetworkResult.Loading -> {
+                        _state.update {
+                            it.copy(
+                                isSimilarProductLoading = true
+                            )
+                        }
+                    }
+
+                    is NetworkResult.Success -> {
+                        _state.update {
+                            it.copy(
+                                similarProductList = res.data?.data ?: emptyList(),
+                                isSimilarProductLoading = false
+                            )
+                        }
+                    }
+
+                    is NetworkResult.Error -> {
+                        _state.update {
+                            it.copy(
+                                message = res.message,
+                                isSimilarProductLoading = false
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
