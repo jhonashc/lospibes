22be16b45a0a716756635e7599e362d1865665d7
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-22 15:34:59 -0500

    home feature updated: get combo by id added in combo service

--- app/src/main/java/com/example/lospibes/features/home/data/source/remote/ComboService.kt
+++ app/src/main/java/com/example/lospibes/features/home/data/source/remote/ComboService.kt
@@ -1,9 +1,11 @@
 package com.example.lospibes.features.home.data.source.remote
 
 import com.example.lospibes.features.home.data.dto.response.ComboResponse
+import com.example.lospibes.features.home.data.dto.response.CombosResponse
 import com.example.lospibes.utils.Constants.COMBOS
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ComboService {
@@ -14,5 +16,10 @@ interface ComboService {
         @Query("max") max: Int? = null,
         @Query("limit") limit: Int? = null,
         @Query("offset") offset: Int? = null
+    ): Response<CombosResponse>
+
+    @GET("${COMBOS}/{id}")
+    suspend fun getComboById(
+        @Path("id") id: String
     ): Response<ComboResponse>
 }
