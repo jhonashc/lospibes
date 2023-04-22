db2cc0a2b739bd77e6da2107a4a30d6cc9f59fc9
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-21 21:02:23 -0500

    home feature updated: combo repository and impl adde

---
+++ app/src/main/java/com/example/lospibes/features/home/data/repository/ComboRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.example.lospibes.features.home.data.repository
+
+import com.example.lospibes.features.home.data.dto.query.GetCombosQueryDto
+import com.example.lospibes.features.home.data.dto.response.ComboResponse
+import com.example.lospibes.features.home.data.source.remote.ComboService
+import com.example.lospibes.features.home.domain.repository.ComboRepository
+import com.example.lospibes.utils.BaseApiResponse
+import com.example.lospibes.utils.NetworkResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class ComboRepositoryImpl @Inject constructor(
+    private val comboService: ComboService
+) : ComboRepository, BaseApiResponse() {
+    override fun getCombos(
+        getCombosQueryDto: GetCombosQueryDto?
+    ): Flow<NetworkResult<ComboResponse>> {
+        return safeApiCall {
+            comboService.getCombos(
+                name = getCombosQueryDto?.name,
+                min = getCombosQueryDto?.min,
+                max = getCombosQueryDto?.max,
+                limit = getCombosQueryDto?.limit,
+                offset = getCombosQueryDto?.offset
+            )
+        }.flowOn(Dispatchers.IO)
+    }
+}
