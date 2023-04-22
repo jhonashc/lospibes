cfb147a675ea48d7837f6bdee44a08f42f4f894c
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-21 21:00:46 -0500

    home feature updated: combo model updated in domain

--- app/src/main/java/com/example/lospibes/features/home/domain/model/Combo.kt
+++ app/src/main/java/com/example/lospibes/features/home/domain/model/Combo.kt
@@ -1,10 +1,14 @@
 package com.example.lospibes.features.home.domain.model
 
+import java.util.Date
+
 data class Combo(
     val id: String,
     val name: String,
     val description: String? = null,
     val price: Number,
     val imageUrl: String? = null,
-    val products: List<Product> = emptyList()
+    val products: List<Product>,
+    val createdAt: Date,
+    val updatedAt: Date
 )
