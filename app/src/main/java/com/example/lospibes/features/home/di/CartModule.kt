c05f4c23fc18a10408f9ea5fd07e572572e7a408
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-24 01:27:06 -0500

    home feature updated: cart module added in di

---
+++ app/src/main/java/com/example/lospibes/features/home/di/CartModule.kt
@@ -0,0 +1,29 @@
+package com.example.lospibes.features.home.di
+
+import com.example.lospibes.features.home.data.repository.CartRepositoryImpl
+import com.example.lospibes.features.home.domain.repository.CartRepository
+import com.example.lospibes.features.home.domain.use_case.cart.AddToCart
+import com.example.lospibes.features.home.domain.use_case.cart.CartUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CartModule {
+    @Singleton
+    @Provides
+    fun provideCartRepository(): CartRepository {
+        return CartRepositoryImpl()
+    }
+
+    @Singleton
+    @Provides
+    fun provideCartUseCase(cartRepository: CartRepository): CartUseCase {
+        return CartUseCase(
+            addToCart = AddToCart(cartRepository)
+        )
+    }
+}
