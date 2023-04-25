13ef6fcee7acb865730c5c48bb9bd11aaa2d0d69
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-24 21:49:27 -0500

    cart repository and impl updated: add and remove from cart, add and subtract quantity and remove all added

--- app/src/main/java/com/example/lospibes/features/home/data/repository/CartRepositoryImpl.kt
+++ app/src/main/java/com/example/lospibes/features/home/data/repository/CartRepositoryImpl.kt
@@ -4,10 +4,49 @@ import com.example.lospibes.features.home.domain.model.CartItem
 import com.example.lospibes.features.home.domain.repository.CartRepository
 
 class CartRepositoryImpl : CartRepository {
-    private val cartList = mutableListOf<CartItem>()
+    private val cartList: MutableList<CartItem> = mutableListOf()
 
     override fun addToCart(cartItem: CartItem): List<CartItem> {
         cartList.add(cartItem)
+        return cartList
+    }
+
+    override fun removeFromCart(cartItem: CartItem): List<CartItem> {
+        return cartList.filter { item -> item.id != cartItem.id }
+    }
+
+    override fun addQuantity(cartItem: CartItem): List<CartItem> {
+        return cartList.map { item ->
+            if (item.id == cartItem.id) {
+                return@map CartItem(
+                    id = item.id,
+                    name = item.name,
+                    imageUrl = item.imageUrl,
+                    price = item.price,
+                    quantity = item.quantity + 1
+                )
+            }
+            return@map item
+        }
+    }
+
+    override fun subtractQuantity(cartItem: CartItem): List<CartItem> {
+        return cartList.map { item ->
+            if (item.id == cartItem.id) {
+                return@map CartItem(
+                    id = item.id,
+                    name = item.name,
+                    imageUrl = item.imageUrl,
+                    price = item.price,
+                    quantity = item.quantity - 1
+                )
+            }
+            return@map item
+        }.filter { item -> item.quantity > 0 }
+    }
+
+    override fun removeAll(): List<CartItem> {
+        cartList.clear()
         return cartList
     }
 }
--- app/src/main/java/com/example/lospibes/features/home/domain/repository/CartRepository.kt
+++ app/src/main/java/com/example/lospibes/features/home/domain/repository/CartRepository.kt
@@ -4,4 +4,12 @@ import com.example.lospibes.features.home.domain.model.CartItem
 
 interface CartRepository {
     fun addToCart(cartItem: CartItem): List<CartItem>
+
+    fun removeFromCart(cartItem: CartItem): List<CartItem>
+
+    fun addQuantity(cartItem: CartItem): List<CartItem>
+
+    fun subtractQuantity(cartItem: CartItem): List<CartItem>
+
+    fun removeAll(): List<CartItem>
 }
